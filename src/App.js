dd55a09809c44fff8741c7dd07aa776093848cdc
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-26 10:46:55 -0700

    - As a user, when I click on one of the question marks instead of the alert the question mark turns into a tree emoji.

--- src/App.js
+++ src/App.js
@@ -16,12 +16,22 @@ const App = () => {
     "?"
   ])
 
+  const handleGamePlay = (clickedSquare) => {
+      let updateBoard = [...board]
+      updateBoard[clickedSquare] = "🌳"
+      setBoard(updateBoard)
+  }
+
   return (
     <>
       <h1>Treasure Hunt Game</h1>
       < div className="board-game">
       {board.map((square, index) => {
-        return <Square square={square} />
+        return <Square 
+                  square={square}  
+                  index={index} 
+                  key={index}
+                  handleGamePlay={handleGamePlay}/>
       })}
     </div>
     </>
