ef62d5e495f441f9c1fac9aaa41382b592ce3281
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-09-26 10:03:25 -0700

    added css, square and READme notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
+import Square from './components/Square'
 import "./App.css"
+
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -17,6 +19,11 @@ const App = () => {
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      < div className="board-game">
+      {board.map((square, index) => {
+        return <Square square={square} />
+      })}
+    </div>
     </>
   )
 }
